6c0ed3413d8238cbbb4e815cad09c7803fd1acf7
papa2kundu-cyber <papa2kundu@gmail.com>
2026-04-09 18:58:48 +0530

    Add files via upload

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,16 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: 'export',
   images: {
     unoptimized: true,
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'brightocityinterior.com',
+        port: '',
+        pathname: '/backend/storage/**',
+      },
+    ],
   },
 };
 
